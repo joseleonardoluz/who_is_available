e9cc420a27556fc909775f7310826650e6ef691c
jlluz <jlluz@rcnradio.com.co>
2021-03-09 11:03:42 -0500

    Creation page of users

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,58 +1,113 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:who_is_available/widgets/diver_custom.dart';
+import 'package:who_is_available/widgets/divider_custom.dart';
 import 'package:who_is_available/widgets/main_background.dart';
 
-class HomePage extends StatelessWidget {
-
+class HomePage extends StatefulWidget {
   final String name;
   final String lastName;
   final String imgUrl;
   final String numberCel;
 
-  const HomePage({
-    Key key, 
-    this.name  = 'Omar', 
-    this.lastName  = 'Paez', 
-    this.imgUrl, 
-    this.numberCel
-    }) : super(key: key);
-  
+  const HomePage(
+      {Key key,
+      this.name = 'Omar',
+      this.lastName = 'Paez',
+      this.imgUrl,
+      this.numberCel})
+      : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  AnimationController controller;
+
+  Animation<double> rotacion;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(41, 32, 77, 1),
         title: Text('¿Quién está disponible? '),
       ),
       body: Stack(children: <Widget>[
         MainBackgound(),
-        Container(          
+        Container(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-
               _Avatar(),
-              
               Container(
-                margin: EdgeInsets.only(left: 15, bottom: 10),
-                child: Column(                  
+                margin: EdgeInsets.only(left: 15, bottom: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text('Nombre: $name',
-                          style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14,
-                          color: Color.fromRGBO(255, 255, 255, 1),),),
-                    SizedBox(height: 5,),
-                    Text('Apellidos: $lastName', 
-                          style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14,
-                          color: Color.fromRGBO(255, 255, 255, 1),),)
+                    Text(
+                      'Nombre: ${widget.name} ${widget.lastName}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Text('Enviar mensaje',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 14,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              )),
+                          ElasticIn(
+                           delay: Duration(milliseconds: 1000),
+                            child: IconButton(
+                              icon: Icon(Icons.mail_outline, color: Colors.white),
+                              onPressed: (){
+
+                              },
+                            )
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Text(
+                            'Llamar: ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14,
+                              color: Color.fromRGBO(255, 255, 255, 1),
+                            ),
+                          ),
+                          ElasticIn(
+                            delay: Duration(milliseconds: 2000),
+                            child: IconButton(
+                              icon: Icon(Icons.call, color: Colors.white),
+                              onPressed: (){
+
+                              },
+                            )
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
-
               DividerCustom.dividerCustom,
-
             ],
           ),
         ),
@@ -69,9 +124,9 @@ class _Avatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 40, bottom: 40),
+      margin: EdgeInsets.only(top: 50, bottom: 100),
       width: MediaQuery.of(context).size.width,
-      height: 125,
+      height: 135,
       child: CircleAvatar(
         backgroundColor: Colors.green,
       ),
